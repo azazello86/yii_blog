3c191e29a461d242a6e407cf65e7dcf1093c75ac
dmitriy <azazello_86@mail.ru>
2019-05-08 10:31:43 +0300

    update admin/article module

---
+++ models/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property int $category_id
+ * @property string $title
+ * @property string $body
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author_id'], 'required'],
+            [['author_id', 'category_id'], 'integer'],
+            [['body'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'author_id' => 'Author ID',
+            'category_id' => 'Category ID',
+            'title' => 'Title',
+            'body' => 'Body',
+        ];
+    }
+}
---
+++ models/Subscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "subscriber".
+ *
+ * @property int $id
+ * @property string $email
+ */
+class Subscriber extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'subscriber';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+        ];
+    }
+}
